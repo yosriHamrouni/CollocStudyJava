0070b5e8be434dcaeb96d2191a9d09b9b7ecd07f
oumeima <oumeyma.sokkeh@esprit.tn>
2024-05-05 14:30:59 +0100

    pdf+pagination

--- src/main/java/gui/AjouterCoworking.java
+++ src/main/java/gui/AjouterCoworking.java
@@ -70,21 +70,6 @@ public class AjouterCoworking {
     private String imagePathInDatabase;
     @FXML
     private ChoiceBox<String> choiceDispo;
-   // public static final String ACCOUNT_SID = "ACafcc6da8edac3b6b83141b831c51f111";
-    public static final String AUTH_TOKEN = "ff28b1ff9e01c0f47cb693813d9f9ea6";
-
-    // Méthode pour envoyer un SMS
-    /*public void sendSMS(String recipientPhoneNumber, String messageBody) {
-       // Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
-
-        Message message = Message.creator(
-                        new PhoneNumber(recipientPhoneNumber),
-                        new PhoneNumber("+14697079006"),
-                        messageBody)
-                .create();
-
-        System.out.println("SMS envoyé avec SID: " + message.getSid());
-    }*/
 
 
     @FXML
